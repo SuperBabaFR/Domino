1d05b0f47ba733c4d0522c3b4241b9c3000de7dd
ackerman667 <75074538+ackerman667@users.noreply.github.com>
2025-02-17 11:59:05 -0400

    rejoindre script
    manque les test

--- Godot_front/connexion.gd
+++ Godot_front/connexion.gd
@@ -16,15 +16,21 @@ func _on_connexion_reussie():
 		#print(body)
 		var headers = ["Content-Type: application/json"]
 		$LoginRequest.request("http://localhost:8000/login", headers, HTTPClient.METHOD_POST, body)
+	else:
+		print("Veuillez remplir tous les champs ") #Afficher le message à l'utilisateur a la place du print
+		
 		
 func _on_traiter_resultat(result, response_code, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
-	print("dsqddqdqdqdq")
-	print(response)
 	if (response_code == 200):
 		get_tree().change_scene_to_file("res://home_menu.tscn")
+	else:
+		#get_tree().change_scene_to_file("res://connexion.tscn")
+		print(response.message) #Afficher le message à l'utilisateur a la place du print
+		
+		
 		
 		
 	

cfeb7754c776d9818be0a7f1ff26344fc84a500c
SuperBabaFR <78614041+SuperBabaFR@users.noreply.github.com>
2025-02-20 19:20:49 -0400

    Create game scene
    tout les paramètres sont prêts manque juste la méthode

---
+++ Godot_front/Scripts/create_game.gd
@@ -0,0 +1,46 @@
+extends Control
+
+@export var slider_reflexion_time: Slider
+@export var slider_nb_max_player: Slider
+@export var btn_creer: Button
+@export var lab_nb_max_player_value: Label
+@export var lab_reflexion_time_value: Label
+@export var ragequit_penality: CheckButton
+@export var is_public_game: CheckButton
+@export var session_name: LineEdit
+
+func _ready():
+	pass
+
+func _on_slider_nb_max_players_value_changed(value):
+	lab_nb_max_player_value.text = str(slider_nb_max_player.value) + " Joueurs"
+
+func _on_slider_reflexion_time_value_changed(value):
+	lab_reflexion_time_value.text = str(slider_reflexion_time.value) + " Secondes"
+
+func _on_btn_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/home_menu.tscn")
+
+
+func _on_btn_creer_pressed():
+	var session_name = session_name.text
+	var reflexion_time = slider_reflexion_time.value
+	var max_players_count = slider_nb_max_player.value
+	var definitive_leave = ragequit_penality.toggle_mode
+	var is_public = is_public_game.toggle_mode
+	
+	var body = {
+		"reflexion_time": reflexion_time, 
+		"max_players_count": max_players_count, 
+		"definitive_leave":definitive_leave, 
+		"is_public":is_public
+	}
+	
+	if session_name != "":
+		body["session_name"] = session_name
+	
+	var json_body = JSON.stringify(body, "  ")
+	
+	print(json_body)
+	
+	#get_tree().change_scene_to_file("res://Scenes/home_menu.tscn")
--- Godot_front/Scripts/global.gd
+++ Godot_front/Scripts/global.gd
@@ -1,20 +1,24 @@
 extends Node
 
-var user_data = {}  # Stocke les infos utilisateur après connexion
+var player_data = {}  # Stocke les infos utilisateur après connexion
 var is_logged_in = false  # Statut de connexion
 
-func set_user_data(data: Dictionary):
-	user_data = data
+func set_player_data(data: Dictionary):
+	player_data = data
 	is_logged_in = true
-	#print("Utilisateur connecté :", user_data)
+	#print("Utilisateur connecté :", player_data)
 
 func get_user_info(key: String, default_value = null):
-	return user_data.get(key, default_value)
+	return player_data.get(key, default_value)
 
-func get_all_user_data():
-	return user_data
+func get_all_player_data():
+	return player_data
 
 func reset_user():
-	user_data.clear()
+	player_data.clear()
 	is_logged_in = false
 	print("Utilisateur déconnecté")
+
+
+func makeRequest(route, jsonBody):
+	pass

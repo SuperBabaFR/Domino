dce2fa5cf1f837c0cc72bb73a71958bdb3498a96
ackerman667 <75074538+ackerman667@users.noreply.github.com>
2025-02-22 20:15:21 -0400

    liste sessions

--- Godot_front/Scripts/global.gd
+++ Godot_front/Scripts/global.gd
@@ -12,7 +12,7 @@ func set_player_data(data: Dictionary):
 	player_data = data
 	is_logged_in = true
 	token_access_fresh = true
-	print("Joueur connecté :", player_data.pseudo)
+	print("Joueur connecté :", player_data)
 
 func get_player_info(key: String, default_value = null):
 	return player_data.get(key, default_value)
